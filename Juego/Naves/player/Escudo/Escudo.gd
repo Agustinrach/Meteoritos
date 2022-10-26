5d7f6b55e7bfe97030a9e7582c687795053b07f5
Agustinrach <agustinrach00@gmail.com>
2022-10-26 16:44:34 -0300

    escudo pulido

--- Juego/Naves/player/Escudo/Escudo.gd
+++ Juego/Naves/player/Escudo/Escudo.gd
@@ -17,6 +17,8 @@ func get_esta_activado() -> bool:
 
 func _process(delta: float) -> void:
 	energia += radio_desgaste*delta
+	if energia <= 0.0:
+		desactivar()
 
 ##metodos
 
@@ -31,14 +33,27 @@ func controlar_colisionador(esta_Desactivado:bool) -> void:
 	$CollisionShape2D.set_deferred("disabled", esta_Desactivado)
 
 func activar()-> void:
+	if energia <=0.0:
+		return
+	
 	esta_activado = true
 	controlar_colisionador(false)
-	$AnimationPlayer.play("Activando")		
+	$AnimationPlayer.play("Activando")
+	
+func desactivar() -> void:
+	set_process(false)
+	esta_activado = false
+	controlar_colisionador(true)
+	$AnimationPlayer.play_backwards("Activando")			
 
 ##señales internas
 
 func _on_animation_finished(anim_name: String) -> void:
-	if anim_name == "Activando":
+	if anim_name == "Activando" and esta_activado:
 		$AnimationPlayer.play("Activado")
 		set_process(true)
 		
+
+
+func _on_body_entered(body: Node) -> void:
+	body.queue_free()
